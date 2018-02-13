b8f9b791fcae45eda1aa7b94042c006858e383f9
Nicholas Teal <nteal15@gmail.com>
2018-02-13 12:11:02 -0600

    delete all events earlier than given datetime, order event queries, cleaned up code

--- db/index.js
+++ db/index.js
@@ -24,52 +24,59 @@ const Event = sequelize.define('event', {
   description: Sequelize.TEXT,
 });
 
+const { Op } = Sequelize;
+
+
 // create table if it doesn't exist
 Event.sync();
 
-// create function to insert into table
+// insert new instance into table
 // returns a promise, access new event with param in `then` phrase
 // input type should be an object like:
 /**
 {
   address: string (anything less than 255 chars),
-  lat: float(12.123456),
-  long: float(123.123456),
+  lat: float(##.######),
+  long: float(###.######),
   venue: string(can hold many characters),
   date: string(yyyy-mm-dd hh:mm:ss),
   name: string(255 chars),
   description: string(many chars)
 }
  */
-const addEvent = ({ address, lat, long, venue, date, name, description }) => {
-  const latFloat = parseFloat(lat);
-  const longFloat = parseFloat(long);
-  return Event.create({
-    address,
-    lat: latFloat,
-    long: longFloat,
-    venue,
-    date,
-    name,
-    description,
+const addEvent = eventObj => Event.create(eventObj);
+
+
+// query all events in table, organized by date (ascending)
+const fetchAll = () =>
+  Event.findAll({
+    order: [
+      ['date', 'ASC'],
+    ],
   });
-};
 
-// create function to query all events in table
-//TODO: organize by date?
-const findAll = (() => Event.findAll());
+// delete events from table before given date (must be formatted correctly)
+// date format: "yyyy-mm-dd hh:mm:ss"
+// recommend using 00:00:00 for time on date AFTER the day you want everything cleared
+const deleteEvents = date =>
+  Event.destroy({
+    where: {
+      date: {
+        [Op.lt]: date,
+      },
+    },
+  });
 
-
-// basic code to check if sequelize connected correctly
+// check if sequelize connected correctly
 sequelize
   .authenticate()
   .then(() => {
-    console.log('\n~~~~~~Connection has been established successfully.~~~~~~~\n');
+    console.log('Connection has been established successfully.');
   })
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
 
-
-exports.findAll = findAll;
+exports.deleteEvents = deleteEvents;
+exports.fetchAll = fetchAll;
 exports.addEvent = addEvent;
