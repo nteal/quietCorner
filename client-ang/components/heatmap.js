1e64259368cc3184fc4f61927a3db11868232937
Nicholas Teal <nteal15@gmail.com>
2018-02-15 12:08:04 -0600

    added and commented out http requests in preparation for server

--- client-ang/components/heatmap.js
+++ client-ang/components/heatmap.js
@@ -3,14 +3,19 @@ angular.module('app')
     bindings: {
       selectedDate: '<',
     },
-    controller($scope) {
+    controller($scope, $http) {
       const heatmap = this;
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(heatmap.selectedDate) === '[object Date]') {
-          // TODO: make http req to get heatmap for correct date
-          // console.log('selected date is: ', heatmap.selectedDate, '(called in heatmap.js)');
+          // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
+          // $http.post('/heatmap', { body: { date: heatmap.selectedDate } })
+          //   .then((response) => { console.log('heatmap date is', response.data); })
+          //   .catch((err) => { console.log('sorry, got an error trying to get the heat map :/'); });
+
+          // TODO: delete following line before production:
+          console.log('selected date is: ', heatmap.selectedDate, '(called in heatmap.js)');
         } else {
-          console.log('looks like your date is not a date...');
+          console.log('Hmm, looks like the date is not actually a date. Sorry about that! ');
         }
       });
     },
--- client-ang/components/recommends.js
+++ client-ang/components/recommends.js
@@ -3,7 +3,8 @@ angular.module('app')
     bindings: {
       selectedDate: '<',
     },
-    controller($scope, dummyData) {
+    // TODO: delete dummy data from dependencies (and everywhere else)
+    controller($scope, $http, dummyData) {
       const recommendsMod = this;
       this.name = 'name';
       this.image = '';
@@ -11,7 +12,15 @@ angular.module('app')
       this.recommendsArr = [];
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(recommendsMod.selectedDate) === '[object Date]') {
-          // TODO: make http request using recommendsMod.selectedDate instead of following line:
+          // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
+          // $http.post('/recommendations', { body: { date: recommendsMod.selectedDate } })
+          //   // currently expecting response to give back array of objs
+          //   // expecting objs to have name, description, and image url for each event
+          //   // need to update recommendsMod.recommendsArr to be the array of objs you get back
+          //   .then((response) => { console.log('recommends data is:', response.data); })
+          //   .catch((err) => { console.log('sorry, got an error trying to get the recommendations :/'); });
+
+          // TODO: delete following two lines:
           this.recommendsArr = dummyData;
           console.log(recommendsMod.selectedDate);
         } else {
