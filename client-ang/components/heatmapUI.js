cddf3166941512255ece034908da06f13d6f906e
Nicholas Teal <nteal15@gmail.com>
2018-02-15 12:00:43 -0600

    date select textbox readonly (must select from calendar)

--- client-ang/components/heatmapUI.js
+++ client-ang/components/heatmapUI.js
@@ -4,19 +4,21 @@ angular.module('app')
       updateAppDate: '<',
     },
     controller() {
-      // make datepicker
       const heatUiMod = this;
-      this.selectDate = (newDate) => {
-        const selectedDate = new Date(newDate);
-        // update app's selectedDate:
-        this.updateAppDate(selectedDate);
-      };
+
+      // create startDate (current date), minDate, and maxDate for date picker to use:
       this.date = new Date();
       this.dateNum = this.date.getDate();
       this.minDate = new Date();
       this.minDate.setDate(heatUiMod.dateNum - 1);
       this.maxDate = new Date();
       this.maxDate.setDate(heatUiMod.dateNum + 7);
+
+      this.selectDate = (newDate) => {
+        const selectedDate = new Date(newDate);
+        // update app's selectedDate:
+        this.updateAppDate(selectedDate);
+      };
     },
     templateUrl: '/templates/heatmapUI.html',
   });
