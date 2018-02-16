b4d8b7b77deddba47c8dba431302685f5a9f517a
Nicholas Teal <nteal15@gmail.com>
2018-02-16 09:23:28 -0600

    accepts html for heatmap

--- client-ang/components/heatmap.js
+++ client-ang/components/heatmap.js
@@ -3,13 +3,16 @@ angular.module('app')
     bindings: {
       selectedDate: '<',
     },
-    controller($scope, $http) {
+    controller($scope, $http, $sce) {
       const heatmap = this;
+      this.heatmap = $sce.trustAsHtml('<h3>put heatmap here</h3>');
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(heatmap.selectedDate) === '[object Date]') {
           // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
           // $http.post('/heatmap', { body: { date: heatmap.selectedDate } })
-          //   .then((response) => { console.log('heatmap date is', response.data); })
+          //   .then((response) => {
+          //     heatmap.heatmap = $sce.trustAsHtml(response.data);
+          //   })
           //   .catch((err) => { console.log('sorry, got an error trying to get the heat map :/'); });
 
           // TODO: delete following line before production:
