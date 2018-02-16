6423bd9a042daf800ea3691ab74ed43183183795
Nicholas Teal <nteal15@gmail.com>
2018-02-16 11:00:00 -0600

    centered recommends

--- client-ang/components/recommends.js
+++ client-ang/components/recommends.js
@@ -6,10 +6,16 @@ angular.module('app')
     // TODO: delete dummy data from dependencies (and everywhere else)
     controller($scope, $http, dummyData) {
       const recommendsMod = this;
+
       this.name = 'name';
       this.image = '';
       this.description = '';
       this.recommendsArr = [];
+
+      // create grid styling for recommendations:
+      this.getGridWidth = index => (index === 1 ? 'su-col-2' : 'su-col-edge-2');
+
+      // make http req whenever selectedDate is updated
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(recommendsMod.selectedDate) === '[object Date]') {
           // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
