f74b82548dc6910bff5cdcff8e4c5e3af6b742ba
Jay Kindell <jaykindell@gmail.com>
2018-02-17 11:09:43 -0600

    adds songkick api & db insert, updates pkg json

--- api-helpers/helpers.js
+++ api-helpers/helpers.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const request = require('request');
-
+// const converter = require('xml-js');
 const db = require('../db/index.js');
 
 
@@ -16,26 +16,41 @@ const db = require('../db/index.js');
 //       *****don't forget to add image url. eventObj.image_url****** DONE
 // }
 
-const getEventfulEvents = (date) => {
-  const options = {
+
+const songkickFormatForDatabase = (resultArray) => {
+  resultArray.forEach((event) => {
+    const formattedEvent = {
+      lat: event.venue.lat,
+      long: event.venue.lng,
+      date: event.start.date,
+      name: event.displayName,
+      description: event.type,
+      num_people: event.popularity * 100,
+    };
+    db.addEvent(formattedEvent);
+  });
+};
+
+const getSongkickEvents = (date) => {
+  const skOptions = {
     method: 'GET',
-    url: 'http://api.eventful.com/rest/events/search',
-    qs:
-      {
-        location: 'new%20orleans,%20la',
-        date: 'Today',
-        app_key: 'kJPdZJwbJBPj6NhX',
-      },
-    headers:
-      {
-        'Cache-Control': 'no-cache',
-      },
+    url: 'http://api.songkick.com/api/3.0/events.json',
+    qs: {
+      apikey: 'apvszgjjkV8KDNVN',
+      location: 'sk:11772',
+      min_date: '2018-02-16',
+      max_date: '2018-02-18',
+    },
+    headers: {
+      'Cache-Control': 'no-cache',
+    },
   };
 
-  request(options, (error, response, body) => {
+  request(skOptions, (error, response, body) => {
     if (error) throw new Error(error);
-
-    console.log(body);
+    const sParsed = JSON.parse(body);
+    // console.log(sParsed.resultsPage.results.event); // there's your array
+    songkickFormatForDatabase(sParsed.resultsPage.results.event); // there's your array
   });
 };
 
@@ -49,6 +64,7 @@ const yelpFormatForDatabase = (resultArray) => {
       name: eventObj.name,
       description: eventObj.description,
       image_url: eventObj.image_url,
+      num_people: eventObj.attending_count,
     };
     db.addEvent(formattedObj);
   });
@@ -81,3 +97,4 @@ const getYelpEvents = (date) => {
 };
 
 module.exports.getYelpEvents = getYelpEvents;
+module.exports.getSongkickEvents = getSongkickEvents;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,40 @@
+require('dotenv').config();
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const seq = require('../db/index');
 const helpers = require('../api-helpers/helpers');
+// const xmlparser = require('express-xml-bodyparser');
+const request = require('request');
+
 
 // set PORT to correct port to listen to
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// app.use(bodyParser(JSON));
+// get some sweet bodyParser action
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // serve client-ang/index.html on initial page load
 app.use(express.static('client-ang'));
 
+// route to load points from db for heatmap
+app.post('/', (req, res) => {});
 
+// route for yelp api call
+// helpers format & add to db
 app.get('/yelps', (req, res) => {
   helpers.getYelpEvents(new Date());
-  console.log(res);
-  // res.send('we in ger');
+  res.header(200).send('ok, added yelps');
+});
+
+// route for songkick api call
+// helpers format & add to db
+app.get('/songkicks', (req, res) => {
+  helpers.getSongkickEvents(new Date());
+  res.header(200).send('ok, added kix');
 });
 
 
