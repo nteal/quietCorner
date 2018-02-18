c30097f34fb0e834c299f1970450a994f209f4fb
Nicholas Teal <nteal15@gmail.com>
2018-02-18 17:46:10 -0600

    includes google maps

--- client-ang/components/heatmap.js
+++ client-ang/components/heatmap.js
@@ -5,7 +5,18 @@ angular.module('app')
     },
     controller($scope, $http, $sce) {
       const heatmap = this;
+
       this.heatmap = $sce.trustAsHtml('<h3>put heatmap here</h3><h3>put heatmap here</h3><h3>put heatmap here</h3><h3>put heatmap here</h3>');
+      this.heatCoords = [];
+
+      let nola = new google.maps.LatLng(29.956541124848762, -90.07314188983946);
+      let map = new google.maps.Map(document.getElementById('map'), {
+        center: nola,
+        zoom: 13,
+        mapTypeId: 'satellite',
+      });
+
+
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(heatmap.selectedDate) === '[object Date]') {
           // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
