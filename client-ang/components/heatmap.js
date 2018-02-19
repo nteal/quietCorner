d565fdc2ac850f7aeca22891cafe02745220389c
Nicholas Teal <nteal15@gmail.com>
2018-02-18 21:44:52 -0600

    creates initial heatmap when correct api url and key is added

--- client-ang/components/heatmap.js
+++ client-ang/components/heatmap.js
@@ -16,16 +16,33 @@ angular.module('app')
         zoom: 12.5,
       });
 
+      this.heatmapLayer = new google.maps.visualization.HeatmapLayer({
+        data: heatmap.heatCoords,
+      });
+      this.heatmapLayer.setMap(map);
+
+
 
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(heatmap.selectedDate) === '[object Date]') {
           // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
-          // $http.post('/heatmap', { date: heatmap.selectedDate })
-          //   .then((response) => {
-          //     console.log(response.data);
-          //     heatmap.heatmap = $sce.trustAsHtml(response.data);
-          //   })
-          //   .catch((err) => { console.log('sorry, got an error trying to get the heat map :/'); });
+          $http.post('/heatmap', { date: heatmap.selectedDate })
+            .then((response) => {
+              // expect response.data to be array of objs with lat, long, and num_people properties
+              console.log(response.data);
+
+              // 
+              heatmap.heatmapLayer.setMap(null);
+              heatmap.heatCoords = response.data.map((coordinates) => {
+                return {location: new google.maps.LatLng(coordinates.lat, coordinates.long), weight: coordinates.num_people}
+              })
+              heatmap.heatmapLayer = new google.maps.visualization.HeatmapLayer({
+                data: heatmap.heatCoords,
+              });
+              heatmap.heatmapLayer.setMap(map);
+    
+            })
+            .catch((err) => { console.log('sorry, got an error trying to get the heat map :/'); });
 
           // TODO: delete following line before production:
           console.log('selected date is: ', heatmap.selectedDate, '(called in heatmap.js)');
