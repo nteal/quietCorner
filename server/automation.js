b3ee1f2e2feb39ea593f7bef94798b75f2a3c220
Jay Kindell <jaykindell@gmail.com>
2018-02-18 23:19:14 -0600

    adds cron file

---
+++ server/automation.js
@@ -0,0 +1,32 @@
+const CronJob = require('cron').CronJob;
+const helpers = require('../api-helpers/helpers');
+
+// add songkick events at 1 second
+const addSongkickEvents = new CronJob({
+  cronTime: '01 00 00 * * 0-6',
+  onTick() {
+    helpers.getSongkickEvents();
+  },
+  start: true,
+  timeZone: 'America/Los_Angeles',
+});
+
+// add yelp events at 30 seconds
+const addYelpEvents = new CronJob({
+  cronTime: '30 00 00 * * 0-6',
+  onTick() {
+    helpers.getYelpEvents();
+  },
+  start: true,
+  timeZone: 'America/Chicago',
+});
+
+// start jobs
+addSongkickEvents.start();
+addYelpEvents.start();
+
+
+// check if jobs are running
+
+console.log('addSongKickEvent status', addSongkickEvents.running);
+console.log('addYelpEvent status', addYelpEvents.running);
