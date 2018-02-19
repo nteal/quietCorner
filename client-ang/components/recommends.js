4ad5182ae50bedb2446900d986ed59369a62ceec
Nicholas Teal <nteal15@gmail.com>
2018-02-18 22:35:46 -0600

    redners app for at least 1 day

--- client-ang/components/recommends.js
+++ client-ang/components/recommends.js
@@ -19,15 +19,20 @@ angular.module('app')
       $scope.$watch('$ctrl.selectedDate', () => {
         if (Object.prototype.toString.call(recommendsMod.selectedDate) === '[object Date]') {
           // TODO: uncomment following lines, update endpoint, and use response data correctly in `then` statement
-          // $http.post('/recommendations', { date: recommendsMod.selectedDate })
-          //   // currently expecting response to give back array of objs
-          //   // expecting objs to have name, description, and image url for each event
-          //   // need to update recommendsMod.recommendsArr to be the array of objs you get back
-          //   .then((response) => { console.log('recommends data is:', response.data); })
-          //   .catch((err) => { console.log('sorry, got an error trying to get the recommendations :/'); });
+          $http.post('/recommend', { date: recommendsMod.selectedDate })
+            // currently expecting response to give back array of objs
+            // expecting objs to have name, description, and image url for each event
+            // need to update recommendsMod.recommendsArr to be the array of objs you get back
+            .then((response) => { 
+              console.log('recommends data is:', response.data);
+              this.recommendsArr = response.data.map((recommend) => {
+                return {image: recommend.img_url, name: recommend.name, description: recommend.description}
+              });
+             })
+            .catch((err) => { console.log('sorry, got an error trying to get the recommendations :/'); });
 
           // TODO: delete following two lines:
-          this.recommendsArr = dummyData.slice(0, 3);
+          // this.recommendsArr = dummyData.slice(0, 3);
           console.log(recommendsMod.selectedDate);
         } else {
           console.log('looks like your selected date is not a date :/');
